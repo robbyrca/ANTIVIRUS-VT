94faf59f2c564de8f57ec09083c55c1fb72177a6
robbyrca <116655222+robbyrca@users.noreply.github.com>
2023-01-12 15:19:33 +0100

    Recuperar los archivos del miniproyecto
    Ahora hay que configurarlos para el proyecto de verdad.

---
+++ filereportV1.py
@@ -0,0 +1,136 @@
+import shutil,os,requests,json, time
+from urllib import response
+from pathlib import Path
+
+file_source = '/Users/ruben/Documents/GitHub/antivirus/id/'
+file_destination1 = '/Users/ruben/Documents/GitHub/antivirus/verificado/'
+file_destination2 = '/Users/ruben/Documents/GitHub/antivirus/procesandoid/'
+file_destination3 = '/Users/ruben/Documents/GitHub/antivirus/cuarentena/'
+file_destination4 = '/Users/ruben/Documents/GitHub/antivirus/logs/'
+file_destination5 = '/Users/ruben/Documents/GitHub/antivirus/procesandorep/'
+file_here = '/Users/ruben/Documents/GitHub/antivirus/'
+timesleepcount=0
+
+def upload(id):
+    global bucle
+    global timesleepcount
+    url = "https://www.virustotal.com/api/v3/analyses/"+id
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
+    }
+    timesleepcount = timesleepcount + 1
+    if timesleepcount == 5:
+        print('Control de tiempo de 60 segundos')
+        logtime()
+        time.sleep(60)
+        timesleepcount=0
+    else:
+        response = requests.get(url, headers=headers)
+        if(response.status_code == 429):
+            print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+            print("Codigo de error : " + str(response.status_code))
+            exit()
+        if response.status_code == 200:
+            #print(response.text)
+            jsonresp = response.json()
+            if jsonresp.get("data").get("attributes").get("status") != "queued":  
+                malget = jsonresp.get("data").get("attributes").get("stats").get("malicious")
+                #print (malget)
+                if  malget>0:
+                    #print('entro m')
+                    print(enviocheck)
+                    print('Archivo malicioso detectado!')
+                    filepath = os.path.join(root, filename)
+                    shutil.move(filename, file_destination3)
+                    logvir(filename)
+                    os.remove(rutaid)
+                    bucle = True
+                else:
+                    #print('entro r')
+                    #print(enviocheck+'1')
+                    filepath = os.path.join(root, filename)
+                    shutil.move(filename, file_destination1)
+                    log(filename)
+                    os.remove(rutaid)
+                    bucle = True
+
+        else:
+            print ("No s'ha pogut obtenir la URL :(")
+            print ("ERROR al pujar el archiu :!")
+            print ("Status code: " + str(response.status_code))
+
+
+def checkFileExistance(enviocheck):
+    try:
+        with open(enviocheck, 'r') as f:
+            return True
+    except FileNotFoundError as e:
+        return False
+    except IOError as e:
+        return False
+
+def logvir(file):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(filename + " Archivo malicioso. Se ha desplazado a cuarentena\n")
+def log(file):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(filename + " Archivo correcto. Desplazado a verificado\n")
+def logtime():
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write("Control de tiempo de 60 segundos\n")
+def logpy():
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write("\n#filereportV1.py\n")
+
+logpy()
+for root, dirs, files in os.walk(file_source):
+    for filename in files:
+        filepath = os.path.join(root, filename)
+        enviocheck = filepath
+        idenruta = checkFileExistance (enviocheck)
+        if idenruta == True:
+            if filepath != ('/Users/ruben/Documents/GitHub/antivirus/id/.DS_Store'):
+                with open(filepath, 'r') as r:
+                    contenido = r.read()
+                contsplit = contenido.split(":")
+                idcont = contsplit[1]
+                filename1 = contsplit [0]
+                filename2 = filename1.split('"')
+                filename = filename2 [1]
+                idesp = idcont.split('"')
+                id = idesp[0]
+                #print(id)
+                rutaid = filepath
+                enviocheck = filename
+                existe = checkFileExistance(enviocheck)
+                if existe == True:
+                    if filepath != ('/Users/ruben/Documents/GitHub/antivirus/id/.DS_Store'):
+                        bucle=False
+                        while bucle == False:
+                            upload(id)
+                    else:
+                        os.remove(filepath)
+                        print(filepath)
+                        print('removed')
+                else:
+                    print ('No se ha encontrado el archivo')
+                    os.remove(rutaid)
+            else:
+                print(filepath)
+                print('removed')
+                os.remove(filepath)
+        else:
+            print('No hay ningun archivo a analizar')
+
+count=0
+
+for path in os.listdir(file_destination2):
+    if os.path.isfile(os.path.join(root, filename)):
+            count = count + 1
+
+#print('Quedan ' + str(count) + ' archivos por analizar')
+
+if count > 0:
+    if filepath != (file_source+'.DS_Store'):
+        print('\n'+'Quedan ' + str(count) + ' archivos por analizar')
---
+++ sendapiV3.py
@@ -0,0 +1,152 @@
+import shutil,os,requests,json,time
+from urllib import response
+
+timesleepcount=0
+
+file_source = '/Users/ruben/Documents/GitHub/antivirus/virustotal/'
+file_destination1 = '/Users/ruben/Documents/GitHub/antivirus/procesandoid/'
+file_destination2 = '/Users/ruben/Documents/GitHub/antivirus/verificado/'
+file_destination3 = '/Users/ruben/Documents/GitHub/antivirus/cuarentena/'
+file_destination4 = '/Users/ruben/Documents/GitHub/antivirus/id/'
+file_destination5 = '/Users/ruben/Documents/GitHub/antivirus/procesandorep/'
+file_here = '/Users/ruben/Documents/GitHub/antivirus/'
+
+def upload(file):
+    global timesleepcount
+    global bucle
+    url = "https://www.virustotal.com/api/v3/files"
+    files = {"file": open(file, "rb")}
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
+    }
+    timesleepcount = timesleepcount + 1
+    if timesleepcount == 5:
+        print('Control de tiempo de 60 segundos')
+        logtime()
+        time.sleep(60)
+        timesleepcount=0
+    else:
+        response = requests.post(url, files=files, headers=headers)
+        if(response.status_code == 429):
+            print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+            print("Codigo de error : " + str(response.status_code))
+            exit()
+        if response.status_code == 200:
+            jsonresp = response.json()
+            idget = jsonresp.get("data").get("id")
+            print(file_destination1+filename + " sended")
+            shutil.move(file_destination1+filename, file_destination5)
+            idsave(idget,file_destination5+filename)
+            log(filename)
+            bucle = True
+        else:
+            print ("No s'ha pogut obtenir la URL :(")
+            print ("ERROR al pujar el archiu :!")
+            print ("Status code: " + str(response.status_code))
+
+def uploadbig(file):
+        global timesleepcount
+        global buclebig
+        files = {"file": open(file, "rb")}
+        url = "https://www.virustotal.com/api/v3/files/upload_url"
+        headers = {
+            "accept": "application/json",
+            "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
+        }
+        timesleepcount = timesleepcount + 1
+        if timesleepcount == 5:
+            print('Control de tiempo de 60 segundos')
+            logtime()
+            time.sleep(60)
+            timesleepcount=0
+
+        else:
+            response = requests.get(url, headers=headers)
+            if(response.status_code == 429):
+                print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+                print("Codigo de error : " + str(response.status_code))
+                exit()
+
+            if response.status_code == 200:
+                result = response.json()
+                url_upload = result.get("data")
+
+            else:
+                print ("No s'ha pogut obtenir la URL :(")
+                print ("ERROR al pujar el archiu :!")
+                print ("Status code: " + str(response.status_code))
+        
+            #Obtenim una id
+            response = requests.post(url_upload, files=files, headers=headers)
+            if(response.status_code == 429):
+                print("Error de cuota excedida :! o Error de demasiadas solicitudes controlate ;)")
+                print("Codigo de error : " + str(response.status_code))
+                exit()
+
+            if response.status_code == 200:
+                result = response.json()
+                idbig = result.get("data").get("id")
+                print(file_destination1+filename + " sended")
+                shutil.move(file_destination1+filename, file_destination5)
+                idsave(idbig,file_destination5+filename)
+                logbig(filename)
+                buclebig=True
+
+def idsave(id,file):
+    with open(file_destination4+id, "w") as fp:
+        json.dump(file+":"+id, fp, indent=2)
+
+def logbig(file):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(file_destination1+filename + " sended\n")
+def logbigp(filename):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(file_destination1+filename + " processing\n")        
+def log(file):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(file_destination1+filename + " sended\n")
+def logp(filename):
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write(file_destination1+filename + " processing\n")
+def logtime():
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write("Control de tiempo de 60 segundos\n")
+def logpy():
+    with open(file_here+'logsfile', "a") as fp:
+        fp.write("\n#sendapiV3.py\n")
+
+def checkFileExistance(enviocheck):
+    try:
+        with open(enviocheck, 'r') as f:
+            return True
+    except FileNotFoundError as e:
+        return False
+    except IOError as e:
+        return False
+
+logpy()
+for root, dirs, files in os.walk(file_source):
+    for filename in files:
+        filepath = os.path.join(root, filename)
+        shutil.move(filepath, file_destination1)
+for root, dirs, files in os.walk(file_destination1):
+    for filename in files:
+        if (os.path.getsize(os.path.join(root, filename)) >> 20) > 32:  
+            print("\n"+file_destination1+filename + " processing")
+            logbigp(filename)
+            buclebig = False
+            while buclebig == False:
+                uploadbig(file_destination1+filename)
+        else:
+            print("\n"+file_destination1+filename + " processing")
+            logp(filename)
+            bucle = False
+            while bucle == False:
+                upload(file_destination1+filename)
+
+shutil.rmtree(file_source)
+os.mkdir(file_source)
+
+#exec(open("filereportV1.py").read())
+
